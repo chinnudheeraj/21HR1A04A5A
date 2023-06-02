0fbe4c8a4d81f46a5cf8cc3f4bc0f3d1f3c11121
chinnudheeraj <131236145+chinnudheeraj@users.noreply.github.com>
2023-06-02 19:29:31 +0530

    Add files via upload

---
+++ ascii.java
@@ -0,0 +1,12 @@
+import java.util.*;
+class Program207
+{
+public static void main(String args[])
+{
+char ch;
+Scanner sc=new Scanner(System.in);
+ch=sc.next().charAt(0);
+int x=(int)ch;
+System.out.println("ASCII of"+ch+"is"+x);
+}
+}
