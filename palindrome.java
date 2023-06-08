d9a899e323c7e94cb34c98adb39afb9307f9a540
chinnudheeraj <131236145+chinnudheeraj@users.noreply.github.com>
2023-06-08 16:30:50 +0530

    Add files via upload

--- palindrome.java
+++ palindrome.java
@@ -2,16 +2,19 @@ import java.util.Scanner;
 class reversepalindrome{
 public static void main(String args[]){
 Scanner scob=new Scanner(System.in);
-int n;
-int rp=0;
-temp;
-System.out.println("enter any positive integer");
-n=scob.nextInt();
-while(n>0)
+String n;
+String rs="";
+System.out.println("enter a string:");
+n=scob.next();
+int len=n.length()-1;
+for(int i=len;i>=0;i--)
 {
-n=n%10;
-rp=rp*10+n;
-}
-System.out.println(rd);
+rs=rs+n.charAt(i);}
+if(n.equals(rs))
+{
+System.out.println("palindrome");}
+else
+{
+System.out.println("not palindrome");}
 }
 }
